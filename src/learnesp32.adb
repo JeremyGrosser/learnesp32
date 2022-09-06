975cdcbc03cbf8207ebc59abe590552e75abe882
Jeremy Grosser <jeremy@synack.me>
2022-09-05 21:15:26 -0700

    wip

--- src/learnesp32.adb
+++ src/learnesp32.adb
@@ -1,8 +1,9 @@
 with ESP32.UART; use ESP32.UART;
 
 procedure Learnesp32 is
+   Message : constant String := "Hello, stack!";
 begin
    loop
-      Put_Line ("Hello, ESP32!");
+      Put_Line ("Hello, literal!");
    end loop;
 end Learnesp32;
